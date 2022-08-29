9ef7056993d6866e6f580c6c80bff1d6e56db63b
Nanda Puspita Rana <nanda.rana@awalmula.co.id>
2022-08-29 16:56:40 +0700

    refactor: foreach content widget account.move

--- addons/ab_travel_umroh/models/accountmove.py
+++ addons/ab_travel_umroh/models/accountmove.py
@@ -1,4 +1,8 @@
+import json
+import logging
 from odoo import models, fields, api
+
+_logger = logging.getLogger("====models====")
 
 
 class AccountMove(models.Model):
@@ -12,3 +16,10 @@ class AccountMove(models.Model):
 
     def action_cetak_invoice(self):
         return self.env.ref('aa_travel_umroh.report_cetak_invoice_action').report_action(self)
+
+    def get_widget(self):
+        ambil = json.loads(self.invoice_payments_widget)
+        _logger.warning(ambil)
+        if ambil:
+            return ambil['content']
+        return
